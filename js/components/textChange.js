ebdf9a645ecf0ea174c375cddbf08428309d3478
TomasTheDankEngine <tomas.miri@gmail.com>
2021-04-06 14:51:14 +0300

    interim solution H text change

--- js/components/textChange.js
+++ js/components/textChange.js
@@ -2,20 +2,21 @@ var text = ["Elvish Walters.", "A developer."];
 var element = document.getElementById('changingHeroText');
 var counter = 0;
 function textChange() {
-    setTimeout(function() {
     if (counter >= text.length) { counter = 0};
     element.innerHTML = text[counter];
     counter++;
     element.style.opacity = "1";
-    var timerId = setInterval(function() {
-        var opacity = element.style.opacity;
-        if (opacity === 0) {
-            clearInterval(timerId);
-        } else {
-            element.style.opacity = opacity - 0.04;
-        }
-    }, 500);
-}, 5000);
+    $("#changingHeroText").fadeOut(function() {
+        $(this).text("World")
+      }).fadeIn();
+    // var timerId = setInterval(function() {
+    //     var opacity = element.style.opacity;
+    //     if (opacity === 0) {
+    //         clearInterval(timerId);
+    //     } else {
+    //         element.style.opacity = opacity - 0.05;
+    //     }
+    // }, 150);
 }
 
 setInterval(textChange, 5000)
