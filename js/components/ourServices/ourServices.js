b2cc5fa881e2936ac2c73936352506346064085e
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-04-08 22:21:02 +0300

    Update ourServices.js

--- js/components/ourServices/ourServices.js
+++ js/components/ourServices/ourServices.js
@@ -1,34 +1,27 @@
 function ourServices(selector, data) {
 
+
     const DOM = document.querySelector(selector);
-    const {list, maxCount} = data;
+    const ourServicesArray = data.list;
 
     let HTML = '';
-    let generatedOurServicesCount = 0;
 
-    for (let i = 0; i < list.length; i++) {
-        const ourService = list[i];
-        
+    for (let i = 0; i < ourServisesArray.lenght; i++) {
+        const ourService = ourServicesArray[i];
 
         if (!ourService.active) {
             continue;
         }
 
-        if (generatedOurServicesCount === maxCount) {
-            break;
-        }
+        HTML +=  `<div class="col-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 services-item">
+                        <i class="services-icon fa fa-desktop" aria-hidden="true"></i>
+                        <h2>${ourService.title}</h2>
+                        <p>${ourService.description}</p>
+                  </div>`;
 
-        generatedOurServicesCount++;
-        HTML +=  `<div class="services-item">
-                        <i class="services-icon fa fa-${ourService.icon}" aria-hidden="true"></i>
-                        <h2 class="title">${ourService.title}</h2>
-                        <p class="paragraph">${ourService.description}</p>
-                     </div>`;
-    }   
-
+    }
 
     DOM.innerHTML = HTML;
-    
 }
 
 export {ourServices}
