23fe25160ec5475fc4e44433dfa4afc1c2593f5c
TomasTheDankEngine <tomas.miri@gmail.com>
2021-04-08 17:53:29 +0300

    hero #4

--- js/components/textChange.js
+++ js/components/textChange.js
@@ -1,16 +1,9 @@
-$(document).ready(textChange);
-var text = ["Elvish Walters.", "A developer.", "A designer."];
-var element = document.getElementById('changingHeroText');
-var counter = 0;
+const example = ['Elvish Walters.', 'A developer.', 'A designer.'];
+const div = document.getElementById('changingHeroText');
+let i = 0;
 
-function textChange() {
-  if (counter >= text.length) { counter = 0};
-  $("#changingHeroText").fadeIn(5000);
-  element.innerHTML = text[counter];
-  counter++;
-  $("#changingHeroText").fadeOut(5000);
-}
-
-setInterval(textChange, 10000)
+setInterval(function() {
+  div.innerHTML = `<span>${example[i++ % example.length]}</span>`
+}, 8000)
 
 export default textChange
