493a155cf760df775c807b5e21ae6d302ebd47cc
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-04-08 23:13:13 +0300

    Our-services-js changes

--- js/components/ourServices/ourServices.js
+++ js/components/ourServices/ourServices.js
@@ -1,20 +1,24 @@
 function ourServices(selector, data) {
 
-
     const DOM = document.querySelector(selector);
-    const ourServicesArray = data.list;
+    const {list, maxCount} = data;
 
     let HTML = '';
+    let generatedOurServicesCount = 0;
 
-    for (let i = 0; i < ourServisesArray.lenght; i++) {
-        const ourService = ourServicesArray[i];
+    for (let i = 0; i < list.length; i++) {
+        const ourService = list[i];
 
         if (!ourService.active) {
             continue;
         }
 
+        if (generatedOurServicesCount === maxCount) {
+            break;
+        }
+
         HTML +=  `<div class="col-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 services-item">
-                        <i class="services-icon fa fa-desktop" aria-hidden="true"></i>
+                    <i class="services-icon fa fa-${ourService.icon}" aria-hidden="true"></i>
                         <h2>${ourService.title}</h2>
                         <p>${ourService.description}</p>
                   </div>`;
