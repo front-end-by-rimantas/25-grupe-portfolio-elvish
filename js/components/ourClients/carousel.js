1d14013a2b219d89fd611d7f00577670aa67fbda
juosta <79445286+juosta@users.noreply.github.com>
2021-04-10 21:37:37 +0300

    carousel (without automated sliding)

--- js/components/ourClients/carousel.js
+++ js/components/ourClients/carousel.js
@@ -1,5 +1,17 @@
-function carousel() {
+let activeSliderIndex = 0;
 
+function carousel() {
+    const sliderButtons = document.querySelectorAll('.slider');
+    const list = document.querySelector('.reviewsList');
+    for(let i = 0; i < sliderButtons.length; i++){
+        const slider = sliderButtons[i];
+        slider.addEventListener('click', () => {
+            sliderButtons[activeSliderIndex].classList.remove('sliderActive');
+            activeSliderIndex = i;
+            slider.classList.add('sliderActive');
+            list.style.marginLeft = `-${i}00%`;
+        });
+    }
 }
 
 export { carousel }
