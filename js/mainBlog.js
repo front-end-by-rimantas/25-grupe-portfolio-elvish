81d691c972ae25f272e86394aab8bb474c24be24
juosta <79445286+juosta@users.noreply.github.com>
2021-04-13 17:46:41 +0300

    render categories and archives

--- js/mainBlog.js
+++ js/mainBlog.js
@@ -4,7 +4,11 @@
 //import { ourBlogs } from './components/ourBlogs/ourBlogs.js'
 import { renderPages } from './components/blog-lifestyle/renderPages.js'
 // search column
-
+import { renderCategories } from './components/blog-lifestyle/renderCategories.js'
+import { categories } from './data/blogData/categoriesData.js';
+import { archives } from './data/blogData/archivesData.js';
+import { recentPosts } from './data/blogData/recentPostsData.js';
+import { tags } from './data/blogData/tagsData.js';
 // footer
 import { footerData } from './data/footerData.js';
 import { footerRender } from './components/footer/footerRender.js';
@@ -16,5 +20,9 @@ import { footerRender } from './components/footer/footerRender.js';
 //ourBlogs('#blog_posts_block', ourBlogsData);
 renderPages('#pages', 4);
 document.querySelector('.pageNumber').classList.add("selected");
+// search column
+renderCategories('#categories', categories, "Categories");
+
+renderCategories('#archives', archives, "Archives");
 // footer
 footerRender('#footer-icons-blog', footerData)
