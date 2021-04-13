8d364bb8c49445112d6281efb236c93a986878bf
Tomas <tomas.v.mob@gmail.com>
2021-04-14 02:26:59 +0300

    Email send on Mouse click

--- js/components/emailsend/sendEmail.js
+++ js/components/emailsend/sendEmail.js
@@ -1,17 +1,28 @@
 document.getElementById('email_sending').addEventListener('click', sendEmail);
 
-let message_receiver = 'tomas.va@yahoo.com';
+document.getElementById('sub-email').addEventListener('keypress', logKey);
 
-function sendEmail(recipient) {
-    recipient = message_receiver;
-    console.log(`Jūs paspaudėte knopką. Gavėjas: ${recipient}`);
+function logKey(e) {
+  console.log(`${e.code}`);
+  if (e.code === 'Enter') {
+      sendEmail();
+  }
+}
+
+function sendEmail() {
+    let recipient = document.getElementById('sub-email').value;
+    console.log('Gavėjas: ', recipient);
+    if (recipient === '') {
+        return;
+    }
+    console.log(`Jūs paspaudėte Subscribe knopką. Gavėjas: ${recipient}`);
     Email.send({
       Host: 'smtp.gmail.com',
       Username: 'bit25.Webpage@gmail.com',
       Password: 'bit25Meskiniukas',
       To: recipient,
       From: 'bit25.Webpage@gmail.com',
-      Subject: 'Subscription for the Elvish Webpage has been added successfully 2021-04-12--488',
+      Subject: 'Subscription for the Elvish Webpage has been added successfully 2021-04-12--1x118',
       Body: `Subscription for the Elvish Webpage has been added for ${recipient}.<br><br>Thank you!<br><br><br>© BIT-25 Team Elvish.`,
     })
       .then(function (message) {
