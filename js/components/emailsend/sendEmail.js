3fcf58941076897e237694a1c753df9b946866aa
Tomas <tomas.v.mob@gmail.com>
2021-04-13 17:01:00 +0300

    Tmp fix

--- js/components/emailsend/sendEmail.js
+++ js/components/emailsend/sendEmail.js
@@ -1,4 +1,4 @@
-document.getElementById("email_sending").addEventListener("click", sendEmail);
+document.getElementById('email_sending').addEventListener('click', sendEmail);
 
 let message_receiver = 'tomas.va@yahoo.com';
 
@@ -6,16 +6,16 @@ function sendEmail(recipient) {
     recipient = message_receiver;
     console.log(`Jūs paspaudėte knopką. Gavėjas: ${recipient}`);
     Email.send({
-      Host: "smtp.gmail.com",
-      Username: "bit25.Webpage@gmail.com",
-      Password: "bit25Meskiniukas",
+      Host: 'smtp.gmail.com',
+      Username: 'bit25.Webpage@gmail.com',
+      Password: 'bit25Meskiniukas',
       To: recipient,
-      From: "bit25.Webpage@gmail.com",
-      Subject: "Subscription for the Elvish Webpage has been added successfully 2021-04-12--422",
+      From: 'bit25.Webpage@gmail.com',
+      Subject: 'Subscription for the Elvish Webpage has been added successfully 2021-04-12--488',
       Body: `Subscription for the Elvish Webpage has been added for ${recipient}.<br><br>Thank you!<br><br><br>© BIT-25 Team Elvish.`,
     })
       .then(function (message) {
-        alert("mail sent successfully")
+        alert('mail sent successfully')
       });
   }
 
