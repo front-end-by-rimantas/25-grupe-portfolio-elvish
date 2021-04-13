a1ec28d9a4f0260b52d0341a2dc95d7a9c957d19
juosta <79445286+juosta@users.noreply.github.com>
2021-04-13 17:02:49 +0300

    render blog, page numbers

--- js/mainBlog.js
+++ js/mainBlog.js
@@ -1,6 +1,8 @@
 // IMPORT
 // blog
-
+//import { ourBlogsData } from './data/ourBlogsData.js'
+//import { ourBlogs } from './components/ourBlogs/ourBlogs.js'
+import { renderPages } from './components/blog-lifestyle/renderPages.js'
 // search column
 
 // footer
@@ -10,5 +12,9 @@ import { footerRender } from './components/footer/footerRender.js';
 
 // FUNKCIJU PANAUDOJIMAS
 
+// blog posts
+//ourBlogs('#blog_posts_block', ourBlogsData);
+renderPages('#pages', 4);
+document.querySelector('.pageNumber').classList.add("selected");
 // footer
 footerRender('#footer-icons-blog', footerData)
