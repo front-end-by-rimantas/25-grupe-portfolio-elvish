17a88e8d45fa040f9175e0303e1918e74476246d
juosta <79445286+juosta@users.noreply.github.com>
2021-04-13 15:29:59 +0300

    blog footer

---
+++ js/mainBlog.js
@@ -0,0 +1,10 @@
+// IMPORT
+// footer
+import { footerData } from './data/footerData.js';
+import { footerRender } from './components/footer/footerRender.js';
+// back to top button
+
+// FUNKCIJU PANAUDOJIMAS
+
+// footer
+footerRender('#footer-icons-blog', footerData)
