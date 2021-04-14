e64037de7f60d78059b38c40aee3d2ef47e3433c
Tomas <tomas.v.mob@gmail.com>
2021-04-14 03:32:56 +0300

    Subscribe - send Email basic functionality Update

--- js/components/emailsend/sendEmail.js
+++ js/components/emailsend/sendEmail.js
@@ -23,7 +23,7 @@ function sendEmail() {
       Password: 'bit25Meskiniukas',
       To: recipient,
       From: 'bit25.Webpage@gmail.com',
-      Subject: 'Subscription for the Elvish Webpage has been added successfully 2021-04-12--8888',
+      Subject: 'Subscription for the Elvish Webpage has been added successfully',
       Body: `Subscription for the Elvish Webpage has been added for ${recipient}.<br><br>Thank you!<br><br><br>© BIT-25 Team Elvish.`,
     })
     .then(function (message) {
